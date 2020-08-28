506abb958dd3b46c7e01c50b1b8bb6301d347b95
yrenard-dedalus <53218215+yrenard-dedalus@users.noreply.github.com>
2020-08-28 10:25:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ var app = express();
 var server_port = process.env.SERVER_PORT || 8080;
 var server_ip = process.env.SERVER_IP || '0.0.0.0';
 
-app.get('/Organization', function(req, res) {
-    res.sendFile('./resources/organization.json', {root: __dirname});
-});
-
 app.get('/jwks.json', function(req, res){
     res.sendFile('./resources/jwks.json', {root: __dirname});
 });
